644ad4076bc13e3d843582c6f6842084a22a94a8
ninigordiashvili <gordiashvilinini@gmail.com>
2026-03-17 19:03:35 +0400

    redesigned Review page (#29)

---
+++ src/assets/icons/ShieldIcon.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Svg, { G, Path, Defs, ClipPath } from "react-native-svg";
+
+function ShieldIcon() {
+  return (
+    <Svg
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <G
+        clipPath="url(#clip0_213_2469)"
+        stroke="#3C6300"
+        strokeWidth={1.33266}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <Path d="M13.326 8.662c0 3.332-2.332 4.997-5.104 5.964a.666.666 0 01-.446-.007c-2.779-.96-5.111-2.625-5.111-5.957V3.998a.666.666 0 01.666-.667c1.333 0 2.999-.8 4.158-1.812a.78.78 0 011.013 0c1.166 1.02 2.825 1.812 4.158 1.812a.667.667 0 01.666.667v4.664z" />
+        <Path d="M5.997 7.996L7.33 9.329l2.665-2.666" />
+      </G>
+      <Defs>
+        <ClipPath id="clip0_213_2469">
+          <Path fill="#fff" d="M0 0H15.992V15.992H0z" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+}
+
+export default ShieldIcon;
